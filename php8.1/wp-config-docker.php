db11c79e214690ab803182c8984568165af211e6
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-12-24 08:55:26 -0800

    seems to work now as expected

--- php8.1/wp-config-docker.php
+++ php8.1/wp-config-docker.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * The base configuration for WordPress
+ * The base configuration for ClassicPress
  *
  * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
  * and fill in the values.
  *
  * This file contains the following configurations:
@@ -13,56 +13,48 @@
  * * Database table prefix
  * * ABSPATH
  *
- * This has been slightly modified (to read environment variables) for use in Docker.
+ * @link https://docs.classicpress.net/user-guides/editing-wp-config-php/
  *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
- *
- * @package WordPress
+ * @package ClassicPress
  */
 
-// IMPORTANT: this file needs to stay in-sync with https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
-// (it gets parsed by the upstream wizard in https://github.com/WordPress/WordPress/blob/f27cb65e1ef25d11b535695a660e7282b98eb742/wp-admin/setup-config.php#L356-L392)
-
 // a helper function to lookup "env_FILE", "env", then fallback
-if (!function_exists('getenv_docker')) {
+if ( ! function_exists( 'getenv_docker' ) ) {
+
 	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
-	function getenv_docker($env, $default) {
-		if ($fileEnv = getenv($env . '_FILE')) {
-			return rtrim(file_get_contents($fileEnv), "\r\n");
-		}
-		else if (($val = getenv($env)) !== false) {
+	function getenv_docker( $env, $default ) {
+
+		if ( $fileEnv = getenv( $env . '_FILE' ) ) {
+			
+			return rtrim( file_get_contents ($fileEnv ), "\r\n" );
+		} else if ( ( $val = getenv( $env ) ) !== false ) {
+			
 			return $val;
-		}
-		else {
+		} else {
+
 			return $default;
 		}
 	}
 }
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('CLASSICPRESS_DB_NAME', 'wordpress') );
+/** The name of the database for ClassicPress */
+define( 'DB_NAME', getenv_docker( 'CLASSICPRESS_DB_NAME', 'classicpress' ) );
 
 /** Database username */
-define( 'DB_USER', getenv_docker('CLASSICPRESS_DB_USER', 'example username') );
+define( 'DB_USER', getenv_docker( 'CLASSICPRESS_DB_USER', 'username' ) );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv_docker('CLASSICPRESS_DB_PASSWORD', 'example password') );
-
-/**
- * Docker image fallback values above are sourced from the official WordPress installation wizard:
- * https://github.com/WordPress/WordPress/blob/1356f6537220ffdc32b9dad2a6cdbe2d010b7a88/wp-admin/setup-config.php#L224-L238
- * (However, using "example username" and "example password" in your database is strongly discouraged.  Please use strong, random credentials!)
- */
+define( 'DB_PASSWORD', getenv_docker( 'CLASSICPRESS_DB_PASSWORD', 'password' ) );
 
 /** Database hostname */
-define( 'DB_HOST', getenv_docker('CLASSICPRESS_DB_HOST', 'mysql') );
+define( 'DB_HOST', getenv_docker( 'CLASSICPRESS_DB_HOST', 'mysql' ) );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', getenv_docker('CLASSICPRESS_DB_CHARSET', 'utf8') );
+define( 'DB_CHARSET', getenv_docker( 'CLASSICPRESS_DB_CHARSET', 'utf8' ) );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', getenv_docker('CLASSICPRESS_DB_COLLATE', '') );
+define( 'DB_COLLATE', getenv_docker( 'CLASSICPRESS_DB_COLLATE', '' ) );
 
 /**#@+
  * Authentication unique keys and salts.
@@ -75,34 +67,40 @@ define( 'DB_COLLATE', getenv_docker('CLASSICPRESS_DB_COLLATE', '') );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv_docker('CLASSICPRESS_AUTH_KEY',         'put your unique phrase here') );
-define( 'SECURE_AUTH_KEY',  getenv_docker('CLASSICPRESS_SECURE_AUTH_KEY',  'put your unique phrase here') );
-define( 'LOGGED_IN_KEY',    getenv_docker('CLASSICPRESS_LOGGED_IN_KEY',    'put your unique phrase here') );
-define( 'NONCE_KEY',        getenv_docker('CLASSICPRESS_NONCE_KEY',        'put your unique phrase here') );
-define( 'AUTH_SALT',        getenv_docker('CLASSICPRESS_AUTH_SALT',        'put your unique phrase here') );
-define( 'SECURE_AUTH_SALT', getenv_docker('CLASSICPRESS_SECURE_AUTH_SALT', 'put your unique phrase here') );
-define( 'LOGGED_IN_SALT',   getenv_docker('CLASSICPRESS_LOGGED_IN_SALT',   'put your unique phrase here') );
-define( 'NONCE_SALT',       getenv_docker('CLASSICPRESS_NONCE_SALT',       'put your unique phrase here') );
-// (See also https://wordpress.stackexchange.com/a/152905/199287)
+define( 'AUTH_KEY',         getenv_docker( 'CLASSICPRESS_AUTH_KEY',         'put your unique phrase here' ) );
+define( 'SECURE_AUTH_KEY',  getenv_docker( 'CLASSICPRESS_SECURE_AUTH_KEY',  'put your unique phrase here' ) );
+define( 'LOGGED_IN_KEY',    getenv_docker( 'CLASSICPRESS_LOGGED_IN_KEY',    'put your unique phrase here' ) );
+define( 'NONCE_KEY',        getenv_docker( 'CLASSICPRESS_NONCE_KEY',        'put your unique phrase here' ) );
+define( 'AUTH_SALT',        getenv_docker( 'CLASSICPRESS_AUTH_SALT',        'put your unique phrase here' ) );
+define( 'SECURE_AUTH_SALT', getenv_docker( 'CLASSICPRESS_SECURE_AUTH_SALT', 'put your unique phrase here' ) );
+define( 'LOGGED_IN_SALT',   getenv_docker( 'CLASSICPRESS_LOGGED_IN_SALT',   'put your unique phrase here' ) );
+define( 'NONCE_SALT',       getenv_docker( 'CLASSICPRESS_NONCE_SALT',       'put your unique phrase here' ) );
 
 /**#@-*/
 
 /**
- * WordPress database table prefix.
+ * ClassicPress database table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
- *
- * At the installation time, database tables are created with the specified prefix.
- * Changing this value after WordPress is installed will make your site think
- * it has not been installed.
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
-$table_prefix = getenv_docker('CLASSICPRESS_TABLE_PREFIX', 'wp_');
+$table_prefix = getenv_docker( 'CLASSICPRESS_TABLE_PREFIX', 'cp_' );
 
 /**
- * For developers: WordPress debugging mode.
+ * ClassicPress File Editor
+ * 
+ * The File Editor for plugins and themes is disabled in new installations.
+ * If you want to enable the File Editor, simply remove the line below or
+ * set it to "false".
+ * 
+ * @link https://docs.classicpress.net/user-guides/using-classicpress/editing-files/
+ * 
+ * @since CP-2.0.0
+ */
+define( 'DISALLOW_FILE_EDIT', true );
+
+/**
+ * For developers: ClassicPress debugging mode.
  *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
@@ -111,20 +109,22 @@ $table_prefix = getenv_docker('CLASSICPRESS_TABLE_PREFIX', 'wp_');
  * For information on other constants that can be used for debugging,
  * visit the documentation.
  *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ * @link https://docs.classicpress.net/user-guides/debugging-in-classicpress/
  */
-define( 'WP_DEBUG', !!getenv_docker('CLASSICPRESS_DEBUG', '') );
+define( 'WP_DEBUG', !!getenv_docker( 'CLASSICPRESS_CP_DEBUG', '' ) );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
 // see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && strpos( $_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false ) {
+	
 	$_SERVER['HTTPS'] = 'on';
 }
-// (we include this by default because reverse proxying is extremely common in container environments)
 
-if ($configExtra = getenv_docker('CLASSICPRESS_CONFIG_EXTRA', '')) {
+// (we include this by default because reverse proxying is extremely common in container environments)
+if ( $configExtra = getenv_docker( 'CLASSICPRESS_CONFIG_EXTRA', '' ) ) {
+
 	eval($configExtra);
 }
 
